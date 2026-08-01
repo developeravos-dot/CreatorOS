5954034eb7bb5e465a0b758e070acca10c776328
Your Name <you@example.com>
2026-08-01 23:05:45 +0400

    feat(api): initialize CreatorOS data engine

---
+++ apps/web/src/api/data-engine/index.ts
@@ -0,0 +1,6 @@
+﻿export * from "./QueryClient";
+export * from "./QueryCache";
+export * from "./QueryContext";
+export * from "./useQuery";
+export * from "./useMutation";
+export * from "./EventBus";
